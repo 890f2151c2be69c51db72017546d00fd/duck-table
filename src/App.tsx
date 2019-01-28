4bd7de0a5d52aad4695beeea76ef771adfa3bc1a
Milan Holý <milan.holy@gmail.com>
2019-01-28 17:10:47 +0100

    Init text code

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './App.css'
+
+import DuckTable from './lib'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DuckTable />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/lib/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DuckTable = () => (
+    <div className="duck-table-container">
+       table
+    </div>
+)
+
+export default DuckTable
